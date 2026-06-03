157273c6b4677352ea19ebae830b37d46ec9fcb2
Fredy <fredygochez17@gmail.com>
2026-06-03 16:33:27 -0600

    program can now list data files and stat names from user input

--- scripts/constants.py
+++ scripts/constants.py
@@ -1,6 +1,5 @@
 # CONSTANTS
 
-RAW_DATA_PATH = "data/raw/fc_barcelona_24_25_champions_league_raw_csv_data/"
 DATA_FILE_NAMES = \
 (
     "Advanced_Goalkeeping_2024-2025_Barcelona__Champions_League.csv"    ,
@@ -15,6 +14,9 @@ DATA_FILE_NAMES = \
     "Shooting_2024-2025_Barcelona__Champions_League.csv"                ,
     "Standard_Stats_2024-2025_Barcelona__Champions_League.csv" 
     
+    # other data files... not sure how I'll process them yet
     #"League_phase_2024-2025__Champions_League.csv"                      ,  
     #"Scores_and_Fixtures_2024-2025_Barcelona__Champions_League.csv"     ,        
 )
+
+RAW_DATA_PATH = "data/raw/fc_barcelona_24_25_champions_league_raw_csv_data/"
--- scripts/process_user_inputs.py
+++ scripts/process_user_inputs.py
@@ -9,9 +9,27 @@
 #       [data_file]|[stat]| -list top [integer]
 #       [data_file]|[stat]|[player_name_1], [palyer_name_2] -compare
 
-import time
 
+
+import time
+import csv
+
+
+
+######################## CONSTANTS ########################
+
+# data
 from scripts.constants import DATA_FILE_NAMES
+from scripts.constants import RAW_DATA_PATH
+
+
+##################### END OF CONSTANTS ####################
+
+
+
+
+
+
 
 
 
@@ -23,7 +41,7 @@ def print_valid_inputs():
     time.sleep(0.5)
     
     print('list of valid inputs (mind syntax):')
-    print('\tdata files -list')
+    print('\t-list data files')
     print('\t[data_file] -list stats')
     print('\t[data_file] -list players')
     print('\t[data_file]|[stat]|[player_name]')
@@ -43,13 +61,60 @@ def print_data_files():
     
     for data_file_name in DATA_FILE_NAMES:
         
-        # process data file name
+        # format data file name
         data_file_name = data_file_name.split('_')
         data_file_name = data_file_name[:data_file_name.index('2024-2025')]
         
-        
+        # print formatted file name
         time.sleep(0.15)
         print('\t', " ".join(data_file_name))
+        
+
+def load_data_file(file_name): 
+    
+    file_name = file_name.replace(' ', '_')
+    
+    # find DATA_FILE_NAMES index for which file_name exists
+    for name in DATA_FILE_NAMES:
+        if file_name in name:
+            file_name = name
+            break
+    
+    # open file
+    file_path   = RAW_DATA_PATH + file_name
+    file_handle = open(file_path)
+    
+    # create csv dictionary reader
+    csv_dict_reader = csv.DictReader(file_handle)
+    
+    
+    # build nested dictionary: data[player name as row][stat label as column]
+    data = {}
+    for row in csv_dict_reader:
+        
+        # player name becomes the main key
+        player_name = row['Player']
+        data[player_name] = row
+    
+    
+    return data
+
+
+def print_stat_list(data):
+    print()
+    for player in data:
+        for stat in data[player]:
+            print(stat)
+        
+        print()
+        break
+        
+
+def print_input_error_message():
+    
+    print('One or more bad inputs (check syntax)')
+    print("Try 'help' for a list of valid inputs or 'quit' to exit program")
+    print()
 
 ################################### HELPER FUNCTIONS ####################################
 
@@ -68,19 +133,28 @@ def print_data_files():
 ##################################### MAIN SCRIPT #######################################
 def process_user_inputs(input):
     
-    input = input.split('|')
-    if len(input) == 1:
-        if 'data files -list' in input:
+    if 'help' in input:
+        print_valid_inputs()
+    elif '-list' in input and '|' not in input:
+        if '-list data files' in input:
             print_data_files()
-        elif 'help' in input:
-            print_valid_inputs()
-        else:
-            print('One or more bad inputs (check syntax)')
-            print("Try 'help' for a list of valid inputs or 'quit' to exit program")
-            print()
-    elif len(input) == 2:
-        return
-        
+        elif '-list stats' in input:
+            input = input.split('-')
+            file_name = input[0].strip()
+            
+            # load data
+            data = load_data_file(file_name)
+            
+            # print stat list
+            print_stat_list(data)
+            
+        elif '-list players' in input:
+            print(input)
+        else: print_input_error_message()
+    else:
+        print_input_error_message()
+    
+    
         
 
 
