df378e103023537fbadcf728b35177fd5689b237
Fredy <fredygochez17@gmail.com>
2026-06-17 11:06:30 -0600

    improved processing for quit command

--- main.py
+++ main.py
@@ -4,7 +4,25 @@ import time
 from scripts.process_user_inputs              import process_user_inputs
 
 
+def get_user_input() :
+    print()
+    time.sleep(0.1)
+    user_input = input('Input: ')
+    print(user_input)
+    return user_input
 
+def input_is_quit(user_input) :
+    user_input = user_input.lower()
+    if user_input == 'quit' or user_input == 'q' : 
+        return True
+    else : 
+        return False
+
+def close_program() :
+        print('Closing Program...')
+        time.sleep(0.5)
+        print()
+        time.sleep(0.1)
 
 
 ###############################################################################
@@ -13,23 +31,18 @@ from scripts.process_user_inputs              import process_user_inputs
 
 
 # keep the program going until user quits
-last_user_input = None
-while last_user_input != 'quit' :
+user_input = None
+while user_input != 'quit' :
     
-    print()
-    time.sleep(0.1)
-    last_user_input = input('Your Input: ')
-    print()
+    user_input = get_user_input()
     
-    if last_user_input == 'q' : last_user_input = 'quit'
-    if last_user_input == 'quit': 
-        print('Closing Program...')
-        time.sleep(0.5)
-        print()
-        time.sleep(0.1)
+    #if user_input == 'q' or user_input == 'Q' or user_input == 'QUIT' : user_input = 'quit'
+    if input_is_quit(user_input) : 
+        user_input = 'quit'
+        close_program()
         continue
     
-    process_user_inputs(last_user_input)
+    process_user_inputs(user_input)
 
 
     
